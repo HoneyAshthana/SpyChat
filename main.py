8bc20297b29d30b2c442fc639af6fc4154038fc1
HoneyAshthana <honey.ashthana1@gmail.com>
2017-08-07 23:23:02 +0530

    Conditional statements added

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 print "Let's get started"
-spy_name = raw_input("provide your name here :")
-spy_salutation = raw_input("what should we call you?")
-# concatination of salutation and name.
-spy_name = spy_salutation + ' ' + spy_name
-print+ 'welcome ' + spy_name + " glad to have you"
+spy_name = raw_input("Provide your name here :")
+if len(spy_name) > 0:
+    print 'welcome ' + spy_name + " glad to have you"
+    if len(spy_name)>0:
+        spy_salutation = raw_input("what should we call you?")
+        if len(spy_salutation)>0:
+        # concatination of salutation and name.
+            spy_name = spy_salutation + " " + spy_name
+            print "Welcome "+ spy_name + '\nWe are glad to have you back'
+        else:
+             print "You did not entered your salutation.Please retry!"
+
+else:
+     print "You did not entered your name. Please retry!"
